c5c6a2f728fe25559abc4d29bb98f847c0cb7867
GabrielCostaMarques <gabrielcostamarques2@gmail.com>
2023-01-23 14:49:46 -0300

    aplicando condições

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento === null) {
+        console.log('Elemento não encontrado');
+    }
+    if(elemento.localName==='audio' && elemento != null){
+        elemento.play()
+    }
 }
+
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
